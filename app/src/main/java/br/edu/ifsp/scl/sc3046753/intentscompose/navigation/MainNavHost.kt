ff03abec904976a4f45323da4c2bdbc5b2306c72
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2026-09-16 19:38:54 -0300

    "feat(navigation): pass current string to add word screen"

--- app/src/main/java/br/edu/ifsp/scl/sc3046753/intentscompose/navigation/MainNavHost.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046753/intentscompose/navigation/MainNavHost.kt
@@ -1,10 +1,14 @@
 package br.edu.ifsp.scl.sc3046753.intentscompose.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import br.edu.ifsp.scl.sc3046753.intentscompose.ui.composable.AddWordScreen
 import br.edu.ifsp.scl.sc3046753.intentscompose.ui.composable.HomeScreen
 
 @Composable
@@ -17,7 +21,24 @@ fun MainNavHost(
         startDestination = Screen.HomeScreen.route
     ) {
         composable(Screen.HomeScreen.route) {
-            HomeScreen()
+            HomeScreen { currentString ->
+                navHostController.navigate(
+                    "${Screen.AddWordScreen.route}/${Uri.encode(currentString)}"
+                )
+            }
+        }
+
+        composable(
+            route = "${Screen.AddWordScreen.route}/{currentString}",
+            arguments = listOf(
+                navArgument("currentString") {
+                    type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
+            AddWordScreen(
+                currentString = backStackEntry.arguments?.getString("currentString") ?: ""
+            )
         }
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/sc3046753/intentscompose/ui/composable/AddWordScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046753/intentscompose/ui/composable/AddWordScreen.kt
@@ -2,38 +2,27 @@ package br.edu.ifsp.scl.sc3046753.intentscompose.ui.composable
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-
 @Composable
-fun HomeScreen(
-    modifier: Modifier = Modifier,
-    onAddWordClick: (String) -> Unit
+fun AddWordScreen(
+    currentString: String,
+    modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier.fillMaxWidth()
     ) {
-            OutlinedTextField(
-            value = "",
+        OutlinedTextField(
+            value = currentString,
             onValueChange = {},
             readOnly = true,
             label = {
-                Text("String")
+                Text("String atual")
             },
             modifier = Modifier.fillMaxWidth()
         )
-
-        Button(
-            onClick = {
-                onAddWordClick("")
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Adicionar palavra")
-        }
     }
 }
